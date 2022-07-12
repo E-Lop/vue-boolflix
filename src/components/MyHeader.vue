6de2a829126e5b2f3271539528ca68c54c3dd3e2
E-Lop <edmondolopez@gmail.com>
2022-07-12 14:56:26 +0200

    chiamata axios con input utente

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -15,15 +15,29 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'MyHeader',
   data() {
     return {
       searchValue: '',
+      //   array che contiene risposta alla ricerca filtrata da input
+      resultArray: [],
+      apiLink:
+        'https://api.themoviedb.org/3/search/movie?api_key=8c2a59c90f2e8f4d2da84becf8da96e9&language=en-US&page=1&include_adult=false&query=',
     };
   },
   methods: {
-    startSearch() {},
+    startSearch() {
+      axios
+        //   il link è fatto da una radice fissa + input utente in cui spazi sostituiti da +
+        .get(this.apiLink + this.searchValue.replace(/\s+/g, '+'))
+        .then((response) => {
+          const singleResult = response.data.results;
+          this.resultArray = singleResult;
+          console.log('array', this.resultArray);
+        });
+    },
   },
 };
 </script>
