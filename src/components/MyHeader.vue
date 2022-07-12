a6e851e94ccaa4dfa488441fe35de13c02dfcfd8
E-Lop <edmondolopez@gmail.com>
2022-07-12 12:57:28 +0200

    installato font roboto +creata componente header

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header_space">
+    <div class="logo"></div>
+    <div class="search_space">
+      <input
+        type="text"
+        id="content_search"
+        name="content_search"
+        placeholder="Effettua una ricerca"
+      />
+    </div>
+    <button>Cerca</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: MyHeader,
+};
+</script>
+
+<style lang="scss" scoped></style>
