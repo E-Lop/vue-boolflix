372d657de0600503fe7df5fb0b18b56e449e064b
E-Lop <edmondolopez@gmail.com>
2022-07-13 12:03:01 +0200

    bandiere al posto della lingua

--- src/components/MyDBCard.vue
+++ src/components/MyDBCard.vue
@@ -4,8 +4,12 @@
     <div class="titolo_originale">
       Titolo originale: {{ desiredCards.original_title }}
     </div>
-    <div class="lingua">
-      Lingua originale: {{ desiredCards.original_language }}
+    <div id="lingua">
+      Lingua originale:
+      <img
+        :src="flagRoot + fixFlag(desiredCards.original_language)"
+        alt="Bandiera flag"
+      />
     </div>
     <div class="voto">Voto: {{ desiredCards.vote_average }}</div>
   </div>
@@ -14,8 +18,32 @@
 <script>
 export default {
   name: 'MyDBCard',
+  data() {
+    return {
+      flagRoot: `https://countryflagsapi.com/svg/`,
+      // flag: this.desiredCards.original_language,
+    };
+  },
   props: {
     desiredCards: Object,
+  },
+  methods: {
+    fixFlag(flag) {
+      if (flag == 'en') {
+        return (flag = 'us');
+      } else if (flag == 'ja') {
+        return (flag = 'jp');
+      } else if (flag == 'hi') {
+        return (flag = 'in');
+      } else if (flag == 'cs') {
+        return (flag = 'cz');
+      } else if (flag == 'ko') {
+        return (flag = 'kr');
+      } else if (flag == 'sv') {
+        return (flag = 'ch');
+      }
+      return flag;
+    },
   },
 };
 </script>
@@ -24,5 +52,11 @@ export default {
 .item_card {
   background: lightgreen;
   margin: 10px;
+  #lingua {
+    img {
+      width: 30px;
+      display: inline-block;
+    }
+  }
 }
 </style>
