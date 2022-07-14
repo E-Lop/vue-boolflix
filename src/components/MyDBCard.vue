467a7bd6551099ac44e861d5c8ce5961e968dd6d
E-Lop <edmondolopez@gmail.com>
2022-07-14 11:54:05 +0200

    milestone 3 completata

--- src/components/MyDBCard.vue
+++ src/components/MyDBCard.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="total">
+    <!-- parte dedicata ai film -->
     <div v-if="desiredMovieCards" class="film">
       <div class="item_card">
         <img
@@ -8,6 +9,7 @@
           :src="posterRoot + desiredMovieCards.poster_path"
           alt=""
         />
+        <!-- fallback per risultati senza immagini -->
         <img
           v-else
           class="poster"
@@ -25,15 +27,34 @@
             alt="Bandiera flag"
           />
         </div>
-        <div class="voto">Voto: {{ desiredMovieCards.vote_average }}</div>
+        <div class="voto">
+          <i
+            v-for="n in getStars(desiredMovieCards.vote_average)"
+            :key="n"
+            class="fa-solid fa-star"
+          ></i>
+          <i
+            v-for="n in 5 - getStars(desiredMovieCards.vote_average)"
+            :key="n"
+            class="fa-regular fa-star"
+          ></i>
+        </div>
       </div>
     </div>
-
+    <!-- parte dedicata alle serie tv -->
     <div v-if="desiredSeriesCards" class="serie">
       <div class="item_card">
         <img
+          v-if="desiredSeriesCards.poster_path"
           class="poster"
           :src="posterRoot + desiredSeriesCards.poster_path"
+          alt=""
+        />
+        <!-- fallback per risultati senza immagini -->
+        <img
+          v-else
+          class="poster"
+          :src="require('../assets/img/no_picture_185x278.png')"
           alt=""
         />
         <div class="titolo">Titolo: {{ desiredSeriesCards.name }}</div>
@@ -47,7 +68,19 @@
             alt="Bandiera flag"
           />
         </div>
-        <div class="voto">Voto: {{ desiredSeriesCards.vote_average }}</div>
+        <div class="voto">
+          Voto:
+          <i
+            v-for="n in getStars(desiredSeriesCards.vote_average)"
+            :key="n"
+            class="fa-solid fa-star"
+          ></i>
+          <i
+            v-for="n in 5 - getStars(desiredSeriesCards.vote_average)"
+            :key="n"
+            class="fa-regular fa-star"
+          ></i>
+        </div>
       </div>
     </div>
   </div>
@@ -60,8 +93,6 @@ export default {
     return {
       flagRoot: `https://countryflagsapi.com/svg/`,
       posterRoot: 'https://image.tmdb.org/t/p/w185',
-      onestar:
-        '<i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i>',
     };
   },
   props: {
@@ -85,6 +116,9 @@ export default {
       }
       return flag;
     },
+    getStars(api_vote) {
+      return Math.round(api_vote / 2);
+    },
   },
 };
 </script>
@@ -105,5 +139,11 @@ export default {
 }
 h2 {
   margin-left: 10px;
+}
+.voto {
+  i {
+    color: gold;
+    font-size: 1.2rem;
+  }
 }
 </style>
